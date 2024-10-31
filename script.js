0c25051b9acb31c77d83d62e5e65800116f20fff
eevarsons <edgars.evarsons@inbox.lv>
2024-10-31 17:22:00 +0000

    Izmaiņas

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-window.onload = function() {
-   taisnsturis();
+window.onload = function () {
+  taisnsturis();
+  var navLinks = document.querySelectorAll('.topnav a');
+  navLinks.forEach(function(link) {
+   link.addEventListener('click', function() {
+     navLinks.forEach(function(link) {
+       link.classList.remove('active');
+     });
+      this.classList.add('active');
+    });
+  });
 }
+
 function taisnsturis() {
   var c = document.getElementById("zimejums");
   var ctx = c.getContext("2d");
   ctx.beginPath();
   ctx.rect(20, 20, 150, 100);
   ctx.lineWidth = 15;
-  ctx.strokeStyle = 'blue';
-  ctx.fillStyle = 'green';
+  ctx.strokeStyle = "blue";
+  ctx.fillStyle = "green";
   ctx.fill();
   ctx.stroke();
 }
@@ -19,8 +29,8 @@ function aplis() {
   ctx.beginPath();
   ctx.arc(200, 250, 50, 0, 2 * Math.PI);
   ctx.lineWidth = 8;
-  ctx.strokeStyle = 'red';
-  ctx.fillStyle = 'yellow'; 
+  ctx.strokeStyle = "red";
+  ctx.fillStyle = "yellow";
   ctx.stroke();
   ctx.fill();
 }
@@ -40,7 +50,7 @@ function linija() {
   ctx.moveTo(0, 0);
   ctx.lineTo(500, 400);
   ctx.lineWidth = 4;
-  ctx.strokeStyle = 'orange';
+  ctx.strokeStyle = "orange";
   ctx.stroke();
 }
 
@@ -49,7 +59,39 @@ function bilde() {
   var ctx = c.getContext("2d");
   var img = document.getElementById("bilde");
   ctx.drawImage(img, 10, 200, 100, 100);
-  ctx.strokeStyle = 'yellow';
+  ctx.strokeStyle = "yellow";
   ctx.lineWidht = 5;
   ctx.strokeRect(10, 200, 100, 100);
+}
+
+function aprekins() {
+  let vards = document.getElementById("vards").value;
+  let pirmais = parseFloat(document.getElementById("a").value);
+  let otrais = parseFloat(document.getElementById("b").value);
+  if (vards === "") {
+    alert("Ievadi vārdu!");
+    console.log("Ievadi skaitli!");
+    return;
+  }
+
+  if (!vards.match(/^\S[a-zA-Zā-žĀ-Ž\s]*$/)) {
+    alert("Ievadi vārdu ar burtiem!");
+    console.log("Ievadi vārdu ar burtiem!");
+    return;
+  }
+
+  if (isNaN(pirmais) || isNaN(otrais)) {
+    alert("Ievadi skaitli!");
+    console.log("Ievadi skaitli!");
+    return;
+  }
+
+  let sum = pirmais + otrais;
+    console.log("Rezultāts ir: " + sum);
+    document.getElementById("rezultats").innerHTML = "Tevi sauc: " + vards + ". <br>Rezultāts ir: " + sum;
+    alert("Rezultāts ir: " + sum);
+}
+
+function atjaunotIetvaru(whitch) {
+  document.getElementById("lapas_saturs").innerHTML = '<'+'object id="lapas" type="text/html" data="'+whitch.href+'"></'+'object>';
 }
